be9b2a56d11f6bc64dc34130261b37ed03923567
Oswaldo32s <oswaldo32gonzalez@gmail.com>
2023-06-15 18:46:50 -0600

    add rotating nav

---
+++ 1-basics/Projects/50daysProjects/rotatingNavigation/script.js
@@ -0,0 +1,14 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+const circle = document.querySelector('.circle-container')
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+    circle.classList.add('show-nav');
+})
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+    circle.classList.remove('show-nav');
+})
